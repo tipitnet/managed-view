009a67a8a634c3cdc53a20364dfc469220d80e25
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2018-05-11 18:37:38 -0300

    Adds internet check on web view url load.

--- Managed View/ViewController.swift
+++ Managed View/ViewController.swift
@@ -9,59 +9,31 @@ import JGProgressHUD
 
 class ViewController: UIViewController, UIWebViewDelegate {
     
-//  let UserDefaults: Foundation.UserDefaults = Foundation.UserDefaults.standard
+    // let UserDefaults: Foundation.UserDefaults = Foundation.UserDefaults.standard
     
     @IBOutlet var webView: UIWebView!
     
     let hud = JGProgressHUD(style: .dark)
     
-    let reachability = Reachability()
-    
-//  Default URL to display in web view
+    // Default URL to display in web view
     var defaultURL = URL(string: "http://maximlink.com/readme")
 
-//  Maintenance mode status
+    // Maintenance mode status
     var MAINTENANCE_MODE = "OFF"
     
-//  Last URL loaded in web view
+    // Last URL loaded in web view
     var lastUrl: URL?
     
-//  Pending URL to load
+    // Pending URL to load
     var url: URL? {
         
         didSet {
             
-            if let reachability = reachability {
-                
-                reachability.listener = { status in
-                    
-                    if status == .reachable {
-                        
-                        if self.lastUrl != self.url {
-                            
-                            self.hud.textLabel.text = "Loading"
-                            
-                            let request = URLRequest(url: self.url!)
-                            
-                            self.webView.loadRequest(request)
-                            
-                            self.lastUrl = self.url
-                            
-                            reachability.listener = nil
-                        }
-                        
-                    } else {
-                        
-                        self.hud.textLabel.text = "Waiting for internet connection"
-                    }
-                    
-                    self.hud.show(in: self.view)
-                }
-            }
+            loadURL()
         }
     }
     
-//  Autonomous Single App Mode (Mode)
+    // Autonomous Single App Mode (Mode)
     var asamStatus:Bool = true
     var asamStatusString:String = ""
 
@@ -71,18 +43,24 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
         webView.delegate = self
         
-        defineWebViewUrl()
+        setUrl()
         
         // observe if App Config pushed from MDM
         NotificationCenter.default.addObserver(forName: UserDefaults.didChangeNotification, object: nil, queue: OperationQueue.main) { _ in
             
-            self.defineWebViewUrl()
-            print ("reload")
+            self.setUrl()
+            
+            print("reload")
         }
     }
     
-    //  Tap Gesture Recognizer (triple tap defined in storyboard)
-    //  Gesture used as interactive method to enable or disable Autonomous Single App Mode
+    override var prefersStatusBarHidden : Bool {
+        
+        return true
+    }
+    
+    // Tap Gesture Recognizer (triple tap defined in storyboard)
+    // Gesture used as interactive method to enable or disable Autonomous Single App Mode
     
     @IBAction func tripleTap(_ sender: AnyObject) {
         
@@ -103,11 +81,10 @@ class ViewController: UIViewController, UIWebViewDelegate {
         print (asamStatus)
         
         // define dialog to user
-        
         let actionSheetController: UIAlertController = UIAlertController(title: "Autonomous Single App Mode is currently\n \(asamStatusString)", message: "Select action", preferredStyle: .actionSheet)
         
        
-        //  Customize user dialog based on current state of ASAM and reguest ASAM state change
+        // Customize user dialog based on current state of ASAM and reguest ASAM state change
         var message:String = "Disabled"
         
         if (asamStatus) {
@@ -116,7 +93,7 @@ class ViewController: UIViewController, UIWebViewDelegate {
         
         setupASAM(enabled: asamStatus, actionSheetController: actionSheetController, message: message)
         
-        //Create and add the Cancel action
+        // Create and add the Cancel action
         let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .destructive) { action -> Void in
             //Just dismiss the action sheet
         }
@@ -159,16 +136,14 @@ class ViewController: UIViewController, UIWebViewDelegate {
         actionSheetController.addAction(asam)
     }
     
-    func defineWebViewUrl() {
+    func setUrl() {
 
         print ("INFO: setupView")
         
-        //  Check for Manged App Config
-        
+        // Check for Manged App Config
         if let ManAppConfig = UserDefaults.standard.dictionary(forKey: "com.apple.configuration.managed") {
             
-            //  Check if MAINTENANCE_MODE key is set
-            
+            // Check if MAINTENANCE_MODE key is set
             if (ManAppConfig["MAINTENANCE_MODE"] != nil) {
                 
                 MAINTENANCE_MODE = String(describing: ManAppConfig["MAINTENANCE_MODE"]!)
@@ -178,37 +153,31 @@ class ViewController: UIViewController, UIWebViewDelegate {
                 MAINTENANCE_MODE = "OFF"
             }
             
-            //  Check if MAINTENANCE_MODE key is set to "ON"
-            
+            // Check if MAINTENANCE_MODE key is set to "ON"
             if (MAINTENANCE_MODE == "ON") {
                 
                 url = URL.init(fileURLWithPath: Bundle.main.path(forResource: "curtain", ofType: "png")!)
                 
-                //  If URL changed since last web view load then load new URL
-                
+                // If URL changed since last web view load then load new URL
                 print ("STATUS: loading maintenacne URL \(url!)")
                 
             } else {
                 
-                //  Check if URL key is set
-                
+                // Check if URL key is set
                 if (ManAppConfig["URL"] != nil) {
                     
                     url = URL(string: String(describing: ManAppConfig["URL"]!))
                     
-                    //  If URL changed since last web view load then load new URL
-                    
+                    // If URL changed since last web view load then load new URL
                     print ("STATUS: loading updated AppConfig URL \(url!)")
                 }
 
                 // If no Manged App Config URL key set then use default URL
-                
                 else {
                     
                     url = defaultURL
                     
-                    //  If URL changed since last web view load then load new URL
-                    
+                    // If URL changed since last web view load then load new URL
                     print ("STATUS: loading default URL \(url!)")
                 }
             }
@@ -220,24 +189,83 @@ class ViewController: UIViewController, UIWebViewDelegate {
             
             url = defaultURL
             
-            //  If URL changed since last web view load then load new URL
-            
+            // If URL changed since last web view load then load new URL
             print ("INFO: Refreshing to \(url!)")
         }
     }
-   
-//  Hide Top Status Bar
-    override var prefersStatusBarHidden : Bool {
+    
+    func loadURL() {
         
-        return true
+        if lastUrl != url {
+        
+            hud.textLabel.text = "Loading"
+            
+            hud.show(in: self.view)
+            
+            checkInternet { internet in
+                
+                if internet {
+                    
+                    let request = URLRequest(url: self.url!)
+                    
+                    self.webView.loadRequest(request)
+                    
+                } else {
+                    
+                    DispatchQueue.main.async {
+                        
+                        self.hud.dismiss()
+                    }
+                    
+                    self.retry()
+                }
+            }
+        }
+    }
+    
+    func retry() {
+        
+        let action = UIAlertAction(title: "Retry", style: .default, handler: { action in
+            
+            self.loadURL()
+        })
+        
+        let alert = UIAlertController(title: "", message: "There is no internet connection", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func checkInternet(completionHandler: @escaping (Bool) -> Void) {
+        
+        let url = URL(string: "http://www.appleiphonecell.com/")
+        
+        var request = URLRequest(url: url!)
+        request.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringLocalAndRemoteCacheData
+        request.timeoutInterval = 3.0
+        
+        let task = URLSession.shared.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            
+            let httpResponse = response as? HTTPURLResponse
+            completionHandler(httpResponse?.statusCode == 200)
+        })
+        
+        task.resume()
     }
     
     // MARK: - UIWebViewDelegate
     
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        
+        hud.dismiss()
+        
+        retry()
+    }
+    
     func webViewDidFinishLoad(_ webView: UIWebView) {
         
-        guard !webView.isLoading else { return }
+        lastUrl = url
         
-        self.hud.dismiss()
+        hud.dismiss()
     }
 }
