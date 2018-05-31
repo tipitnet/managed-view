2af7cee362c12412e1f0a989384a148fb7517105
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2018-05-31 09:06:40 +0200

    Removes ASAM support.

--- Managed View/ViewController.swift
+++ Managed View/ViewController.swift
@@ -32,10 +32,6 @@ class ViewController: UIViewController {
             self.loadWebView()
         }
     }
-    
-    // Autonomous Single App Mode (Mode)
-    var asamStatus:Bool = true
-    var asamStatusString:String = ""
 
     override func viewDidLoad() {
         
@@ -55,83 +51,6 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden : Bool {
         
         return true
-    }
-    
-    // Tap Gesture Recognizer (triple tap defined in storyboard)
-    // Gesture used as interactive method to enable or disable Autonomous Single App Mode
-    
-    @IBAction func tripleTap(_ sender: AnyObject) {
-        
-        // If ASAM is enabled
-        if (UIAccessibilityIsGuidedAccessEnabled() == true ) {
-            
-            asamStatus = true
-            asamStatusString = "ENABLED"
-        }
-        
-        // if ASAM is not enabled
-        else {
-            
-            asamStatus = false
-            asamStatusString = "DISABLED"
-        }
-        
-        print (asamStatus)
-        
-        // define dialog to user
-        let actionSheetController: UIAlertController = UIAlertController(title: "Autonomous Single App Mode is currently\n \(asamStatusString)", message: "Select action", preferredStyle: .actionSheet)
-        
-       
-        // Customize user dialog based on current state of ASAM and reguest ASAM state change
-        var message:String = "Disabled"
-        
-        if (asamStatus) {
-            message = "Enabled"
-        }
-        
-        setupASAM(enabled: asamStatus, actionSheetController: actionSheetController, message: message)
-        
-        // Create and add the Cancel action
-        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .destructive) { action -> Void in
-            //Just dismiss the action sheet
-        }
-        
-        actionSheetController.addAction(cancelAction)
-        
-        // for iPad
-        actionSheetController.popoverPresentationController?.sourceView = view
-
-        // Present dialog to user
-        self.present(actionSheetController, animated: true, completion: nil)
-    }
-    
-    func setupASAM(enabled:Bool, actionSheetController: UIAlertController, message:String) {
-        
-        let asam: UIAlertAction = UIAlertAction(title: message, style: .default) { action -> Void in
-            UIAccessibilityRequestGuidedAccessSession(true) { success in
-                
-                print("INFO: ASAM request to set \(message)")
-                
-                if success {
-                    
-                    print ("ASAM is \(message)")
-                    let asamAlert = UIAlertController(title: "Success", message: "Autonomous Single App Mode is\n\n \(message).", preferredStyle: UIAlertControllerStyle.alert)
-                    asamAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    
-                    self.present(asamAlert, animated: true, completion: nil)
-                    
-                } else {
-                    
-                    print ("INFO: ASAM is not capable.")
-                    let asamAlert = UIAlertController(title: "Autonomous Single App Mode is not supported", message: "This device does not currently support Automonous Single App Mode (ASAM).  ASAM requires the following:\n\n (1) Device is in supervised state.\n\n(2) Configuration profile supporting ASAM for this specific app installed on device.", preferredStyle: UIAlertControllerStyle.alert)
-                    asamAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    
-                    self.present(asamAlert, animated: true, completion: nil)
-                }
-            }
-        }
-        
-        actionSheetController.addAction(asam)
     }
     
     func setUrl() {
